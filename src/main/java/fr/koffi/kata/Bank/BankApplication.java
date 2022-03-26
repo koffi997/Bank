6151c1629c3eff3a47c6c7f8ab063a5306312b93
prosper edah <prospernews97etude@gmail.com>
2022-03-26 14:26:08 +0100

    start

---
+++ src/main/java/fr/koffi/kata/Bank/BankApplication.java
@@ -0,0 +1,13 @@
+package fr.koffi.kata.Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/koffi/kata/Bank/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.koffi.kata.Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
